6dd31cd15d039218777725c9617e1851bfb8b5f7
corraid <107348828+corraid@users.noreply.github.com>
2025-01-01 14:45:52 +0100

    Create directory for plot images.

--- src/mkplots.ps1
+++ src/mkplots.ps1
@@ -1,6 +1,8 @@
 $kst = "..\Kst2\bin\kst2.exe"
+$imgs = "images\"
+New-Item -Path $imgs -Type Directory -Force
 $csvfiles = Get-ChildItem "." -Filter *.csv
 foreach ($f in $csvfiles) {
-	$png = "images\" + $f.Basename + ".png"
+	$png = $imgs + $f.Basename + ".png"
 	&$kst sim_fast_sample.kst -F $f --pngHeight 1500 --pngWidth 1500 --png $png
 }
